1b9719eddbde32c35958e7e2bf205a63ec6ee4a4
dbagley23 <dale@reparteegallery.com>
2015-07-31 18:04:48 -0600

    sdf

---
+++ js/mainCtrl.js
@@ -0,0 +1,12 @@
+var app = angular.module('friendsList');
+app.controller('mainCtrl', function($scope){
+	$scope.friends = [
+		"Eric Timmons",
+		"Joey Chavirra",
+		"Justin Nadauld",
+		"Joe Heaps",
+		"Steevun Lemon",
+		"Bryun Pratt"
+	];
+
+});
